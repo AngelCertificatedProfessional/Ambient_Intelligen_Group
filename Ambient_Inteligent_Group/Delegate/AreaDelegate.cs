cbd92aab6614467ca39be2d93954ff7d5bc84344
Angel Espinoza <angel.el.rey@hotmail.com>
2016-01-26 23:41:47 -0800

    Se modifico el diseno del metodo agregar

---
+++ Ambient_Inteligent_Group/Delegate/AreaDelegate.cs
@@ -0,0 +1,30 @@
+﻿using Ambient_Inteligent_Group.Entidad;
+using Ambient_Inteligent_Group.Facade;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambient_Inteligent_Group.Delegate
+{
+    public class AreaDelegate
+    {
+        public static MySqlDataReader BuscarDescripcion(String tabla)
+        {
+            return AreaFacade.BuscarDescripcion(tabla);
+        }
+
+        public static bool ModificarArea(Area area)
+        {
+            return AreaFacade.ModificarArea(area);
+        }
+
+        public static Boolean GuardarArea(Area area)
+        {
+            return AreaFacade.GuardarArea(area);
+        }
+
+    }
+}
